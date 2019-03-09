896b9d2b488a9635c7e55ee964134047b6047410
Derek Miranda <derek.miranda@ff0000.com>
2019-03-08 17:34:48 -0800

    Fix: click handler was calling play() method multiple times
    - caused animations to speed up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-const FRAME_RATE = 16;
-const INIT_SPREAD_RATE = 2;
-const DECAY_RATE = 0.01; // per frame
-const MIN_RATE = 1;
-const RIPPLE_LIMIT = Infinity;
-const RIPPLE_COLOR = "gray";
+const WINDOW_WIDTH_TO_INIT_RATE = 0.02
+const WINDOW_WIDTH_TO_DECAY_RATE = 0.0003
+const WINDOW_WIDTH_TO_MIN_RATE = 0.005
 
-// const queueNextFrame = requestAnimationFrame;
-const queueNextFrame = fn => setTimeout(fn, FRAME_RATE);
+let INIT_SPREAD_RATE = window.innerWidth * WINDOW_WIDTH_TO_INIT_RATE
+let DECAY_RATE = window.innerWidth * WINDOW_WIDTH_TO_DECAY_RATE // per frame
+let MIN_RATE = window.innerWidth * WINDOW_WIDTH_TO_MIN_RATE
+
+const RIPPLE_LIMIT = Infinity;
+
+const queueNextFrame = requestAnimationFrame;
+// const FRAME_RATE = 16;
+// const queueNextFrame = fn => setTimeout(fn, FRAME_RATE);
 
 function generateColorVal() {
   return Math.floor(255 * Math.random());
@@ -35,49 +39,64 @@ class Ripple {
 }
 
 class CanvasRipples {
-  constructor() {
+  constructor(color) {
     this.canvas = document.createElement("canvas");
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
     this.context = this.canvas.getContext("2d");
     this.ripples = [];
+    this.playing = false;
+    this.color = color;
 
-    this.canvas.addEventListener("click", event => {
-      const x = event.clientX;
-      const y = event.clientY;
+    const _touchHandler = this.touchHandler.bind(this)
 
-      if (this.ripples.length < RIPPLE_LIMIT) {
-        const ripple = new Ripple(x, y, RIPPLE_COLOR);
-        this.ripples.push(ripple);
-      }
+    this.canvas.addEventListener("click", _touchHandler);
+    this.canvas.addEventListener("touchend", _touchHandler);
+  }
 
-      const rates = this.ripples.map(ripple => ripple.rate);
-      console.log("rates", rates);
+  touchHandler(event) {
+    const x = event.clientX;
+    const y = event.clientY;
 
+    if (this.ripples.length < RIPPLE_LIMIT) {
+      const ripple = new Ripple(x, y, this.color);
+      this.ripples.push(ripple);
+    }
+
+    const rates = this.ripples.map(ripple => ripple.rate);
+
+    if (!this.playing) {
+      this.playing = true
       this.play();
-    });
+    }
   }
 
   play() {
-    if (this.ripples.length && this.ripples.some(ripple => ripple.radius)) {
+    if (this.ripples.length) {
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      this.ripples.forEach(ripple => {
+        const {
+          x,
+          y,
+          radius,
+          color
+        } = ripple;
+
+        this.context.strokeStyle = color;
+        this.context.beginPath();
+        this.context.arc(x, y, radius, 0, 2 * Math.PI);
+        this.context.stroke();
+        ripple.update();
+      });
+
+      queueNextFrame(this.play.bind(this));
     }
-
-    this.ripples.forEach(ripple => {
-      const { x, y, radius, color } = ripple;
-      this.context.strokeStyle = color;
-      this.context.beginPath();
-      this.context.arc(x, y, radius, 0, 2 * Math.PI);
-      this.context.stroke();
-      ripple.update();
-    });
-
-    queueNextFrame(this.play.bind(this));
   }
 }
 
-(function() {
-  window.canvasRipples = new CanvasRipples();
+(function () {
+  window.canvasRipples = new CanvasRipples('grey');
 
   document.body.appendChild(canvasRipples.canvas);
 })();
