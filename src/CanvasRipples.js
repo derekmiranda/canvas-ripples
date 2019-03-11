b16772a6d0f83764e2eea13595d9fe47f421edf3
Derek Miranda <derek.miranda@ff0000.com>
2019-03-11 11:38:51 -0700

    Removing ripples once offscreen

--- src/CanvasRipples.js
+++ src/CanvasRipples.js
@@ -3,12 +3,37 @@ import Ripple from "./Ripple";
 const RIPPLE_LIMIT = Infinity;
 const queueNextFrame = requestAnimationFrame;
 
+function distBwTwoPoints(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
+}
+
+// ripple is finished whenever radius equals the longest line
+// b/w the ripple origin and either of the canvas' four corners
+function rippleFinished(ripple, canvasWidth, canvasHeight) {
+  const radius = ripple.radius;
+  const canvasMidX = canvasWidth / 2;
+  const canvasMidY = canvasHeight / 2;
+
+  if (radius < Math.max(canvasMidX, canvasMidY)) return false;
+
+  const rippleX = ripple.x;
+  const rippleY = ripple.y;
+
+  // get distance b/w ripple origin and farthest corner
+  const farthestCornerX = rippleX < canvasMidX ? canvasWidth : 0;
+  const farthestCornerY = rippleY < canvasMidY ? canvasHeight : 0;
+  const farthestDist = distBwTwoPoints(
+    rippleX,
+    rippleY,
+    farthestCornerX,
+    farthestCornerY
+  );
+
+  return radius > farthestDist;
+}
+
 class CanvasRipples {
-  constructor({
-    color,
-    redrawCb,
-    lineWidth
-  } = {}) {
+  constructor({ color, redrawCb, lineWidth } = {}) {
     this.canvas = document.createElement("canvas");
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
@@ -18,7 +43,7 @@ class CanvasRipples {
     this.color = color;
     this.redrawCb = redrawCb;
 
-    this.context.lineWidth = lineWidth || 2
+    this.context.lineWidth = lineWidth || 2;
 
     const _touchHandler = this.touchHandler.bind(this);
 
@@ -49,29 +74,35 @@ class CanvasRipples {
     if (this.ripples.length) {
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-      this.ripples.forEach(ripple => {
-        const {
-          x,
-          y,
-          radius,
-          color
-        } = ripple;
+      for (let i = 0; i < this.ripples.length; ) {
+        const ripple = this.ripples[i];
+
+        // remove ripple if going to be offscreen
+        if (rippleFinished(ripple, this.canvas.width, this.canvas.height)) {
+          this.ripples.splice(i, 1);
+          continue;
+        }
+
+        const { x, y, radius, color } = ripple;
 
         this.context.strokeStyle = color;
         this.context.beginPath();
         this.context.arc(x, y, radius, 0, 2 * Math.PI);
         this.context.stroke();
         ripple.update();
-      });
+
+        i += 1;
+      }
 
       // call redraw callback if any
       if (this.redrawCb) {
         this.redrawCb(this.context, this.canvas);
       }
       queueNextFrame(this.play.bind(this));
-      return
+      return;
     }
-    this.playing = false
+
+    this.playing = false;
   }
 }
 
