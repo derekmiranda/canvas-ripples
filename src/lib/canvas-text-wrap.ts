226e0626a55e79852595677ef0129d4281c4265e
Derek Miranda <derek.miranda.1992@gmail.com>
2019-04-19 13:29:49 -0400

    feat: starting on wrapped text rendering

--- src/lib/canvas-text-wrap.ts
+++ src/lib/canvas-text-wrap.ts
@@ -1,24 +1,51 @@
-export interface FillWrapTextOptions {
-  text : string;
-  ctx : CanvasRenderingContext2D;
-  x : number;
-  y : number;
-  width : number;
-  height : number;
-  lineHeight? : number;
+import {CanvasTextWrapper as renderWrappedText} from 'canvas-text-wrapper'
+
+export interface WrappedTextRendererOptions {
+  mainCtx: CanvasRenderingContext2D;
+  x: number;
+  y: number;
+  font: string;
+  width: number;
+  height: number;
+  lineHeight?: number;
 }
 
 let _cachedCanvas
-export function fillTextWithWrapping({
-  text,
-  ctx,
-  x,
-  y,
-  width,
-  height,
-  lineHeight
-} : FillWrapTextOptions) {
-  // _cachedCanvas = _cachedCanvas || document.createElement('canvas')
-  // 
-  ctx.fillText(text, x, y)
+export class WrappedTextRenderer {
+  public x: number;
+  public y: number;
+  public lineHeight: number;
+  public font: string;
+  public mainCtx: CanvasRenderingContext2D;
+  private _wrapCanvas: HTMLCanvasElement;
+  private _wrapCtx: CanvasRenderingContext2D;
+  constructor({
+    mainCtx,
+    font,
+    x,
+    y,
+    width,
+    height,
+    lineHeight
+  } : WrappedTextRendererOptions) {
+    this.x = x
+    this.y = y
+    this.font = font
+    this.mainCtx = mainCtx
+    this._wrapCanvas = document.createElement('canvas')
+    this._wrapCtx = this._wrapCanvas.getContext('2d')
+    this.reformat({ width, height, font })    
+  }
+
+  public reformat({ width, height, font }) : void {
+    if (width) this._wrapCanvas.width = width
+    if (height) this._wrapCanvas.height = height
+    if (font) this._wrapCtx.font = font
+  }
+
+  public render(text) {
+    renderWrappedText(this._wrapCanvas, text, { font: this.font, lineHeight: this.lineHeight })
+    // get image data
+    // write to main context
+  }
 }
--- src/lib/text-render.ts
+++ src/lib/text-render.ts
@@ -1,25 +1,34 @@
-import { fillTextWithWrapping } from './canvas-text-wrap'
+import {WrappedTextRenderer} from './canvas-text-wrap'
 
+const _contentRendererMap = new Map()
 export function renderContentToContext(ctx) : void {
-  const contentCollection : NodeListOf<HTMLElement> = document.querySelectorAll('.content')
-
+  const contentCollection: NodeListOf<HTMLElement> = document.querySelectorAll('.content')
   for (let i = 0; i < contentCollection.length; i += 1) {
     const el : HTMLElement = contentCollection[i]
     const text : string = el.innerText
     const rect : ClientRect = el.getBoundingClientRect()
     const lineHeight : number = parseInt(getComputedStyle(el).lineHeight)
 
+    let textRenderer = _contentRendererMap.get(el)
+    if (!textRenderer) {
+      textRenderer = new WrappedTextRenderer({
+        x: rect.left,
+        y: rect.top,
+        width: rect.width,
+        height: rect.height,
+        mainCtx: ctx,
+        font: ctx.font
+      })
+      _contentRendererMap.set(el, textRenderer)
+    }
+    
+    textRenderer.render(text)
     // hide text
     el.style.color = 'rgba(0, 0, 0, 0)'
 
-    fillTextWithWrapping({
-      text,
-      ctx,
-      x: rect.left,
-      y: rect.top + lineHeight,
-      width: rect.width,
-      height: rect.height,
-      lineHeight
-    })
+    /* TODO:
+    - draw wrapped text on separate canvases
+    - draw image data to main canvas text
+    */
   }
 }
