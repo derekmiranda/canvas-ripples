05a378f1182bbb05e25d695ab6ff6fbb45f3c027
Derek Miranda <derek.miranda.1992@gmail.com>
2019-04-21 18:01:43 -0400

    clean: moving ripple finished util to canvas-utils

--- src/lib/CanvasRipples.ts
+++ src/lib/CanvasRipples.ts
@@ -1,35 +1,7 @@
 import Ripple from "./Ripple";
+import { rippleFinished } from './canvas-utils'
 
 const queueNextFrame = requestAnimationFrame;
-
-function distBwTwoPoints(x1, y1, x2, y2) {
-  return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
-}
-
-// ripple is finished whenever radius equals the longest line
-// b/w the ripple origin and either of the canvas' four corners
-function rippleFinished(ripple: Ripple, canvasWidth, canvasHeight) {
-  const radius = ripple.radius;
-  const canvasMidX = canvasWidth / 2;
-  const canvasMidY = canvasHeight / 2;
-
-  if (radius < Math.max(canvasMidX, canvasMidY)) return false;
-
-  const rippleX = ripple.x;
-  const rippleY = ripple.y;
-
-  // get distance b/w ripple origin and farthest corner
-  const farthestCornerX = rippleX < canvasMidX ? canvasWidth : 0;
-  const farthestCornerY = rippleY < canvasMidY ? canvasHeight : 0;
-  const farthestDist = distBwTwoPoints(
-    rippleX,
-    rippleY,
-    farthestCornerX,
-    farthestCornerY
-  );
-
-  return radius > farthestDist;
-}
 
 interface CanvasRipplesSettings {
   color: string;
